f9e3dc27d956993ca70ae79cb7a972b9ebf44925
Marcos Filipe Ratte Claro <marcosfratte@gmail.com>
2020-04-16 16:57:13 -0300

    Controller/Rote/Form Convenio Criado

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/pareceristas/create','PareceristaController@create');
 Route::post('/pareceristas','PareceristaController@store');
+
+Route::get('/convenios/create','ConvenioController@create');
+Route::post('/convenios','ConvenioController@store');
