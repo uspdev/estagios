f3aff656cd0b5728e6d0180dffda3b0aaeae1676
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-04-16 16:21:39 -0300

    replicado

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Uspdev\Replicado\Pessoa;
+
+class IndexController extends Controller
+{
+    public function index(){
+        /*Arthur: https://github.com/uspdev/replicado/issues/212
+        criar método que verifica se pessoa é Coord Cursos Grad, exemplo:
+        Pessoa::verificaCoordCursosGrad($codpes);
+        exemplo que deve retorna true : 3053989
+        */
+
+        /*
+        Gabriela: https://github.com/uspdev/replicado/issues/213
+        Criar método que verifica se pessoa tem estágio USP ou não, 
+        retorna true ou false
+        exemplo que deve retorna true : você mesmo
+        */
+        dd("index, nada para ver aqui");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/','IndexController@index');
+
 Route::get('/pareceristas/create','PareceristaController@create');
 Route::post('/pareceristas','PareceristaController@store');
