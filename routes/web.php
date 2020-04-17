b1883263d97e144a1693f10140bf872562986cb5
Victor de Oliveira Marinho <vitor.oliveira.marinho@gmail.com>
2020-04-17 14:24:21 -0300

    Implementado Controller/Rota/Form Empresas

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ Route::get('/','IndexController@index');
 
 Route::get('/pareceristas/create','PareceristaController@create');
 Route::post('/pareceristas','PareceristaController@store');
+
+Route::get('/empresas/create', 'EmpresaController@create');
+Route::post('/empresas', 'EmpresaController@store');
