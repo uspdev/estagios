f6c4b5ff7f642aa12cc310f74e75f011c4d743f5
powdn <lauragarcia@usp.br>
2020-04-16 21:56:29 -0300

    Controller/Rota/Form de Avisos

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,8 @@ Route::get('/','IndexController@index');
 
 Route::get('/pareceristas/create','PareceristaController@create');
 Route::post('/pareceristas','PareceristaController@store');
+
+Route::get('/avisos/create','AvisoController@create');
+Route::post('/avisos','AvisoController@store');
+
+
