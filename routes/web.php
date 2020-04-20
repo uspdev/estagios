deca7aeeb05ee11ab33603eb5872c46ce740fd3b
marisabarros11 <marisa.pf.barros@gmail.com>
2020-04-20 13:01:01 -0300

    Controller/Rota/Form Vagas recriados

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/','IndexController@index');
 
-Route::get('/pareceristas/create','PareceristaController@create');
-Route::post('/pareceristas','PareceristaController@store');
+Route::get('/vagas/create','VagaController@create');
+Route::post('/vagas','VagaController@store');
