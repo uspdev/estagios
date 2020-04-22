b220b1e98ce5a3b82574644ed8b42747b598705e
Gabriela Reis <gabrielareisg@hotmail.com>
2020-04-22 13:08:22 -0300

    menu ok

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,19 +9,7 @@ use Uspdev\Replicado\Pessoa;
 class IndexController extends Controller
 {
     public function index(){
-        /*Arthur: https://github.com/uspdev/replicado/issues/212
-        criar método que verifica se pessoa é Coord Cursos Grad, exemplo:
-        Pessoa::verificaCoordCursosGrad($codpes);
-        exemplo que deve retorna true : 3053989
-        */
-        
-        /*
-        Gabriela: https://github.com/uspdev/replicado/issues/213
-        Criar método que verifica se pessoa tem estágio USP ou não, 
-        retorna true ou false
-        exemplo que deve retorna true : você mesmo
-        */
-        dd("index, nada para ver aqui");
+        return view('index');
     }
 
 }
