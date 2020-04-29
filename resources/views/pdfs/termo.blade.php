6d72da3977d373c70412636866921c2b96c7581d
Arthur Ferreira <arthurferreira@usp.br>
2020-04-28 21:12:53 -0300

    Continuação da construção dos PDFs do Termo e Renovação dos estágios

--- resources/views/pdfs/termo.blade.php
+++ resources/views/pdfs/termo.blade.php
@@ -29,14 +29,7 @@
     <b>Letras-Português</b>
 </div>
 
-<br><br><br><br><br><br><br><br><br><br><br><br>
-<hr>
-
-<div style="text-align: initial; font-weight: bold ">
-    OS DOCUMENTOS (TERMO E PLANO)DEVEM SER ENTREGUES PARA ANÁLISE 10 DIAS ÚTEIS ANTES DO INÍCIO DO ESTÁGIO.<br>
-    AO FINAL DE CADA SEMESTRE DE REALIZAÇÃO DO ESTÁGIO DEVERÁ SER ENTREGUE UM RELATÓRIO PESSOAL, NOS TERMOS DA LEI
-    11.788, DA RESOLUÇÃO USP N. 5528.
-</div>
+<p style="page-break-after: always;"></p>
 
 <div style="border-width: 1px; border-style: solid; border-color: #000; text-align: center; padding: 0px;">
     <b>TERMO DE COMPROMISSO</b>
@@ -44,7 +37,7 @@
 
 <div style="text-align: justify;">
     <p style="text-indent : 1em;"><b>Ursa Mãe Primeira Infância Ltda. - EPP, R. Tatuí, 129, Bairro: Jd. Paulista,
-            Cidade: São Paulo - SP - CEP: 01409-010, CNPJ 45.882.982/0001-20</b> , representada por seu(sua)
+    Cidade: São Paulo - SP - CEP: 01409-010, CNPJ 45.882.982/0001-20</b> , representada por seu(sua)
         <b>Coordenadora Geral</b>, Sr(a) <b>Rosinha Cantor Cuperman</b>, adiante designada CONCEDENTE e o(a) ESTAGIÁRIO
         <b>Isis Leocadio Ruiz</b>, estudante, residente a <b>Avenida Professor Mello de Morais, 1235, Bairro: Butantã -
             CEP: 05508-030</b> , na cidade de <b>São Paulo</b>, Estado de <b>SP</b>, portador da cédula de identidade
@@ -58,7 +51,9 @@
         Faculdade, celebram o presente TERMO DE COMPROMISSO DE ESTÁGIO, que se vincula ao convênio para Realização de
         Estágio firmado entre a CONCEDENTE e a INSTITUIÇÃO DE ENSINO nos termos da Lei no 9.394/96 e da Lei no
         11.788/08, conforme as condições a seguir:</p>
-    <p>1. O estágio terá duração de <b>12 meses</b>, a começar em <b>08/05/2020</b> terminando em 07/05/2021 que poderá
+    <p>1. O estágio terá duração de <b>12 meses</b>, a começar em
+        <b>{{ \Carbon\Carbon::parse($estagio->dataini)->format('d/m/Y')}}</b> terminando em
+        <b>{{ \Carbon\Carbon::parse($estagio->datafin)->format('d/m/Y')}}</b> que poderá
         ser eventualmente prorrogado ou modificado por documento complementar <b>(TERMO ADITIVO)</b>.</p>
     <p>1.1. Qualquer das partes poderá pedir rescisão, com 05 (cinco) dias de antecedência.</p>
     <p>1.2. O estagiário não terá vínculo empregatício de qualquer natureza com a CONCEDENTE em razão deste TERMO DE
@@ -72,8 +67,10 @@
         para ser o(a) SUPERVISOR(a) INTERNO(a) do Estágio que será por ele programado.</p>
     <p>4. O ESTAGIÁRIO se obriga a cumprir fielmente a programação do estágio, salvo impossibilidade da qual a
         CONCEDENTE será previamente informada.</p>
-    <p>5. O ESTAGIÁRIO receberá BOLSA DE COMPLEMENTAÇÃO EDUCACIONAL DE <b>R$ 930.00</b> mensais e auxílio-transporte no
-        valor de R$ <b>4.40 diários</b>. A importância referente à bolsa, por não ter natureza salarial, não se enquadra
+    <p>5. O ESTAGIÁRIO receberá BOLSA DE COMPLEMENTAÇÃO EDUCACIONAL DE <b>R$ {{ $estagio->valorbolsa }}</b> {{ $estagio->tipobolsa}} e
+        auxílio-transporte no
+        valor de R$ <b>{{ $estagio->auxtrans }} {{ $estagio->especifiquevt }}</b>. A importância referente à bolsa, por não ter natureza
+        salarial, não se enquadra
         no regime do FGTS (Fundo de Garantia por Tempo de Serviço) e não sofrerá qualquer desconto, inclusive
         previdenciário, exceção feita à retenção do imposto de renda na fonte, quando devido.</p>
     <p>5.1.O estagiário terá direito, sempre que o estágio tenha duração igual ou superior a 1 (um) ano, a um período de
@@ -89,8 +86,9 @@
     <p>7.1 O estagiário declara ter conhecimento e estar de acordo que toda contribuição prática ou intelectual
         desenvolvida em função de suas tarefas como estagiário são de propriedade da Empresa Concedente, não tendo
         direito de subtrair, na totalidade ou em parte, programas, documentos ou arquivos.</p>
-    <p>8. O ESTAGIÁRIO está segurado contra acidentes pessoais, pela Apólice de Seguros <b>nº 820.13818-0071/0000</b>,
-        que está compatível com valores de mercado, da <b>Tokio Marine</b>.</p>
+    <p>8. O ESTAGIÁRIO está segurado contra acidentes pessoais, pela Apólice de Seguros <b>nº
+            {{ $estagio->numseguro }}</b>,
+        que está compatível com valores de mercado, da <b>{{ $estagio->seguradora }}</b>.</p>
     <p>9. O ESTAGIÁRIO deverá informar de imediato e por escrito à CONCEDENTE qualquer fato que interrompa, suspenda ou
         cancele sua matrícula na instituição de Ensino INTERVENIENTE, ficando ele responsável por quaisquer despesas
         causadas pela ausência dessa informação.</p>
@@ -101,7 +99,7 @@
         presença de duas testemunhas para todos os fins e efeitos de direito.</p>
 </div>
 
-<div>São Paulo, 20/04/2020</div>
+<div>São Paulo, {{ Carbon\Carbon::now()->formatLocalized('%d/%m/%Y') }}</div>
 
 <br><br>
 
@@ -124,15 +122,7 @@
     ______________________________________
 </div>
 
-<br><br>
-
-<hr>
-
-<div style="text-align: initial; font-weight: bold; ">
-    OS DOCUMENTOS (TERMO E PLANO)DEVEM SER ENTREGUES PARA ANÁLISE 10 DIAS ÚTEIS ANTES DO INÍCIO DO ESTÁGIO.<br>
-    AO FINAL DE CADA SEMESTRE DE REALIZAÇÃO DO ESTÁGIO DEVERÁ SER ENTREGUE UM RELATÓRIO PESSOAL, NOS TERMOS DA LEI
-    11.788, DA RESOLUÇÃO USP N. 5528.
-</div>
+<p style="page-break-after: always;"></p>
 
 <div style="border-width: 1px; border-style: solid; border-color: #000; text-align: center; padding: 0px;">
     <b>PLANO DE ESTÁGIO</b>
@@ -152,22 +142,18 @@
     Área de atuação da Empresa: <b>Instituição de ensino</b><br>
     Nome do supervisor(a) interno(a) do Estágio na Empresa: <b>Rosinha Cantor Cuperman</b><br>
     Telefone: <b>(11) 991025800</b> / <b>(11) 30827800</b>, E-mail: <b>ursamae@uol.com.br</b><br>
-    Data de início do estágio: <b>08/05/2020</b><br>
-    Data do término do estágio: <b>07/05/2021</b><br>
-    Horário do Estágio: <b>de segunda a sexta-feira, das 9h às 16h, com 1h de intervalo</b><br>
+    Data de início do estágio: <b>{{ \Carbon\Carbon::parse($estagio->dataini)->format('d/m/Y')}}</b><br>
+    Data do término do estágio: <b>{{ \Carbon\Carbon::parse($estagio->datafin)->format('d/m/Y')}}</b><br>
+    Horário do Estágio: <b>{{ $estagio->horario }}</b><br>
     Carga horária semanal: <b>30h00 horas semanais</b><br>
     Duração em meses (em casos excepcionais inferiores a 6 meses, a empresa deverá incluir justificativa circunstanciada
     que será avaliada pelo Supervisor Geral de Estágios): <b>12 meses</b><br>
-    Justificativa:<br>
+    Justificativa:<b><i>{{ $estagio->justificativa }}</i></b><br>
     Número de horas por semana: <b>30h00 horas semanais</b><br>
-    Valor da Bolsa: <b>R$ 930.00 mensais</b><br>
-    Valor do auxílio transporte: R$ <b>4.40 diários</b><br>
+    Valor da Bolsa: R$ <b>{{ $estagio->valorbolsa }} {{ $estagio->tipobolsa }}</b><br>
+    Valor do auxílio transporte: R$ <b>{{ $estagio->auxtrans }} {{ $estagio->especifiquevt }}</b><br>
     Descrição detalhada das atividades a serem desenvolvidas pelo estagiário, com a finalidade de permitir a avaliação
-    da Comissão de Estágios: <br>
-    <b>auxiliar na aplicação e correção de atividades dos alunos; realizar avaliações dos trabalhos escolares; auxiliar
-        com criação de atividades lúdicas e criativas que promovam o desenvolvimento dos alunos; auxiliar no
-        deslocamento dos alunos
-        pela escola</b><br>
+    da Comissão de Estágios: <b><i>{{ $estagio->atividades }}</i></b><br>
     <p><b>NO CASO DE ESTÁGIO DOMICILIAR</b></p>
     <p>Como se dará o controle diário dos horários de início e encerramento das atividades?</p><br>
     <p>Como se dará a supervisão interna (por parte da empresa) do estagiário?</p><br>
@@ -177,7 +163,7 @@
 
 <br>
 
-<div>São Paulo, 20/04/2020</div>
+<div>São Paulo, {{ Carbon\Carbon::now()->formatLocalized('%d/%m/%Y') }}</div>
 
 <br>
 
@@ -191,15 +177,19 @@
     Presidente da Comissão de Graduação da FFLCH/USP
 </div>
 
+<div>
+    <p>Contato: Rosinha Cantor Cuperman , Telefone: (11) 991025800 / (11) 30827800 , E-mail: leticia.moraes@nube.com.br
+    </p>
+</div>
 
-<p>Contato: Rosinha Cantor Cuperman , Telefone: (11) 991025800 / (11) 30827800 , E-mail: leticia.moraes@nube.com.br</p>
+<p style="page-break-after: never;"></p>
 
-<div style="text-align: initial; font-weight: bold; position: absolute; bottom: 0;">
-    <hr>
-    OS DOCUMENTOS (TERMO E PLANO)DEVEM SER ENTREGUES PARA ANÁLISE 10 DIAS ÚTEIS ANTES DO INÍCIO DO ESTÁGIO.<br>
+@endsection('content')
+
+@section('footer')
+<div style="text-align: initial; font-weight: bold;">
+    OS DOCUMENTOS (TERMO E PLANO) DEVEM SER ENTREGUES PARA ANÁLISE 10 DIAS ÚTEIS ANTES DO INÍCIO DO ESTÁGIO.<br>
     AO FINAL DE CADA SEMESTRE DE REALIZAÇÃO DO ESTÁGIO DEVERÁ SER ENTREGUE UM RELATÓRIO PESSOAL, NOS TERMOS DA LEI
     11.788, DA RESOLUÇÃO USP N. 5528.
 </div>
-
-
-@endsection('content')
+@endsection
