d2a82c777d5708befc1a71f5c2207c30bdef9cbd
Marcos Filipe Ratte Claro <marcosfratte@gmail.com>
2020-05-27 14:01:08 -0300

    parte 6 - metodo delete

--- resources/views/convenios/partials/index.blade.php
+++ resources/views/convenios/partials/index.blade.php
@@ -8,7 +8,7 @@
     <table class="table table-striped">
     <thead>
         <tr>
-        <th scope="col">Nome</th>
+        <th scope="col">Nome do Representante</th>
         <th scope="col">Ações</th>
         </tr>
     </thead>
@@ -20,7 +20,17 @@
             <td>
             <a href="/convenios/{{$convenio->id}}/edit"><i class="fas fa-edit"></i></a>
             <a href="/pdfs/convenio/{{$convenio->id}}"><i class="fas fa-file-pdf"></i></a>
+
         </td>
+        <td>
+            <form method="POST" action="/convenios/{{$convenio->id}}">
+                @csrf
+                @method('delete')
+                <button type="submit"><i class="fas fa-trash-alt"></i></button>
+            
+            </form>    
+        </td>
+
         </tr>
         @empty
             <td colspan="2">Não há convênios</td>
