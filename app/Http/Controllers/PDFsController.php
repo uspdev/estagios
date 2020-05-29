a3b4be451f9fa277747080b894c06967de554bc9
Arthur Ferreira <arthurferreira@usp.br>
2020-05-29 12:50:28 -0300

    authorize implementado nas funções termo e renovação

--- app/Http/Controllers/PDFsController.php
+++ app/Http/Controllers/PDFsController.php
@@ -18,9 +18,9 @@ class PDFsController extends Controller
 {
 
     public function termo(Estagio $estagio){
+        $this->authorize('admin');
 
         $empresa = Empresa::where('cnpj',$estagio->cnpj)->first();
-
         // Formata CNPJ
         $empresa->cnpj =  substr($empresa->cnpj, 0, 2) . '.' . substr($empresa->cnpj, 2, 3) . '.' . substr($empresa->cnpj, 5, 3) . '/' . substr($empresa->cnpj, 8, 4) . '-' . substr($empresa->cnpj, 12, 2);
 
@@ -64,6 +64,7 @@ class PDFsController extends Controller
     }
 
     public function renovacao(Estagio $estagio){
+        $this->authorize('admin');
         $pdf = PDF::loadView('pdfs.renovacao', compact('estagio'));
         return $pdf->download('renovacao.pdf');
     }
