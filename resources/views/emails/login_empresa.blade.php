fbadfee24b44eb84300f8340717b0ae4b7ddb072
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-08-26 11:33:00 -0300

    html email

--- resources/views/emails/login_empresa.blade.php
+++ resources/views/emails/login_empresa.blade.php
@@ -1,8 +1,9 @@
 Para realização do login no sistema de estágios da FFLCH clique em:
-
 <a href="{!! $url_login !!}"> Login no sistema </a>
 
+<br><br>
 Ou copie e cole a url no seu navegador:
 {!! $url_login !!}
 
-Mensagem automática - sistema de sites FFLCH
+<br><br>
+Mensagem automática - sistema estágios FFLCH
