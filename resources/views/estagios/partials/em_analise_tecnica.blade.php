110b7ce81e3053b99518f62d6d3b3339de680487
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-08-27 10:26:48 -0300

    Update em_analise_tecnica.blade.php

--- resources/views/estagios/partials/em_analise_tecnica.blade.php
+++ resources/views/estagios/partials/em_analise_tecnica.blade.php
@@ -13,11 +13,11 @@
     </div>
 
     <div class="form-group">
-        <button type="submit" class="btn btn-info" name="analise_tecnica_action" value="indeferimento_analise_tecnica">Indeferir
+        <button type="submit" class="btn btn-info" name="analise_tecnica_action" value="indeferimento_analise_tecnica">Devolver para empresa
         </button>
         <button type="submit" class="btn btn-success" name="analise_tecnica_action" value="deferimento_analise_tecnica"
             onClick="return confirm('Tem certeza que quer enviar para Parecer?')" >
-            Deferir
+            Enviar para parecerista
         </button>
     </div>
 </form>
