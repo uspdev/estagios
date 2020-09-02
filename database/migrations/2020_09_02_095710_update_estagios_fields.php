913f54ef40663f0961dee41fe36233c6d22afbc5
Gabriel de Martin Truzzi <gabriel.truzzi@gmail.com>
2020-09-02 10:05:32 -0300

    Nova migration com correções

---
+++ database/migrations/2020_09_02_095710_update_estagios_fields.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateEstagiosFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('estagios', function (Blueprint $table) {
+            $table->text('atividades')->nullable()->change();
+            $table->text('justificativa')->nullable()->change(); 
+            $table->text('atividadesjustificativa')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('estagios', function (Blueprint $table) {
+            //
+        });
+    }
+}
