8e6884f2c49a7a2956b3b370f50b18737720bccb
Gabriel de Martin Truzzi <gabriel.truzzi@gmail.com>
2020-09-02 11:21:03 -0300

    nova migration

--- database/migrations/2020_09_02_111626_newupdates_fields_estagios.php
+++ database/migrations/2020_09_02_111626_newupdates_fields_estagios.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateEstagiosFields extends Migration
+class NewupdatesFieldsEstagios extends Migration
 {
     /**
      * Run the migrations.
