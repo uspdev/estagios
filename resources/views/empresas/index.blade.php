f9110936d334bd402c1eea7aa0131467e064298a
Gabriel de Martin Truzzi <gabriel.truzzi@gmail.com>
2020-09-04 14:13:27 -0300

    Rollback no sistema index de empresas

--- resources/views/empresas/index.blade.php
+++ resources/views/empresas/index.blade.php
@@ -7,9 +7,21 @@
 @section('content')
 @include('flash')
 
-<br>
 
-<table class="table table-striped datatable" id="index">
+<form method="get" action="/empresas">
+    <div class="row">
+        <div class="col-sm input-group">
+        <input type="text" class="form-control" name="busca" value="{{ Request()->busca }}" placeholder="Busca somente por CNPJ">
+            <span class="input-group-btn">
+                <button type="submit" class="btn btn-success"> Buscar </button>
+            </span>
+        </div>
+    </div>
+</form>
+<br>
+{{ $empresas->appends(request()->query())->links() }}
+<table class="table table-striped" id="index">
+
     <thead>
         <tr>
             <th>Nome da Empresa</th>
