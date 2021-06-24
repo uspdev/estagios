7ea986e6d03a6a882fab6e694638de6cbae3edcd
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-06-24 15:24:27 -0300

    skins ok

--- config/skins.php
+++
@@ -1,8 +0,0 @@
-<?php
-# lista de skins disponíveis no theme
-
-chdir(__DIR__ . '/../resources/views/partials/skins');
-$skins = glob('*', GLOB_ONLYDIR);
-return [
-    'available-skins' => $skins,
-];
