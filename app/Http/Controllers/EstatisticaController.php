4883d7656ea3c2efc562800c48a1a5afa1416440
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-09-14 12:24:50 -0300

    update estatistica controller

--- app/Http/Controllers/EstatisticaController.php
+++ app/Http/Controllers/EstatisticaController.php
@@ -14,108 +14,31 @@ use Carbon\Carbon;
 class EstatisticaController extends Controller
 {
     public function index(Request $request, Estagio $estagio, Empresa $empresa){
+        $cursos = Estagio::whereNotNull('nomcur')->select('nomcur')->distinct()->get()->pluck('nomcur');
 
-        if(isset($request->busca_ano)){
+        if(!$request->start_date) {
+            return view('estatisticas.index')->with([
+                'cursos' => $cursos
+            ]);   
+        }
 
-            $request->validate([
-                'busca_ano' => 'required|numeric',
-            ]);
-            
-            // retorno de variaveis em caso de pesquisa por data
-
-            if ($request->busca_ano != null){
-
-                $total_estagios = Estagio::whereYear('created_at', '=', $request->busca_ano)->count();
-
-                $total_concluidos = Estagio::whereYear('data_inicial', '=', $request->busca_ano)->where('status','=','concluido')->orWhereYear('data_final', '=', $request->busca_ano)->count();
+        $request->validate([
+            'start_date' => 'required',
+            'end_date' => 'required',
+        ]);
         
-                $total_renovados = Estagio::whereYear('created_at', '=', $request->busca_ano)->where('renovacao_parent_id','!=',null)->count();
-        
-                $total_rescindidos = Estagio::whereYear('rescisao_data', '=', $request->busca_ano)->where('status','=','rescisao')->count();
-        
-                $total_empresas = Empresa::whereYear('created_at', '=', $request->busca_ano)->count();
+        $start_date = Carbon::createFromFormat('d/m/Y', $request->start_date)->format('Y-m-d');
+        $end_date = Carbon::createFromFormat('d/m/Y', $request->end_date)->format('Y-m-d');
 
-                $total_bachfilosofia = Estagio::whereYear('created_at', '=', $request->busca_ano)->where('graduacao','=','Bacharelado em Filosofia')->count();
+        $estagios = Estagio::whereBetween('data_inicial', [$start_date, $end_date]);
 
-                $total_licfilosofia = Estagio::whereYear('created_at', '=', $request->busca_ano)->where('graduacao','=','Licenciatura em Filosofia')->count();
-        
-                $total_bachhistoria = Estagio::whereYear('created_at', '=', $request->busca_ano)->where('graduacao','=','Bacharelado em Historia')->count();
-        
-                $total_lichistoria = Estagio::whereYear('created_at', '=', $request->busca_ano)->where('graduacao','=','Licenciatura em Historia')->count();
-        
-                $total_bachgeografia = Estagio::whereYear('created_at', '=', $request->busca_ano)->where('graduacao','=','Bacharelado em Geografia')->count();
-        
-                $total_licgeografia = Estagio::whereYear('created_at', '=', $request->busca_ano)->where('graduacao','=','Licenciatura em Geografia')->count();
-        
-                $total_bachsociais = Estagio::whereYear('created_at', '=', $request->busca_ano)->where('graduacao','=','Bacharelado em Ciencias Sociais')->count();
-        
-                $total_licsociais = Estagio::whereYear('created_at', '=', $request->busca_ano)->where('graduacao','=','Licenciatura em Ciencias Sociais')->count();
-        
-                $total_bachletras = Estagio::whereYear('created_at', '=', $request->busca_ano)->where('graduacao','LIKE',"%"."bacharelado - habilitacao"."%")->count();
-        
-                $total_licletras = Estagio::whereYear('created_at', '=', $request->busca_ano)->where('graduacao','LIKE',"%"."licenciatura - habilitacao"."%")->count();
-        
-                $total_basicoletras = Estagio::whereYear('created_at', '=', $request->busca_ano)->where('graduacao','=','Letras - Ciclo Básico')->count();
-
-            }
-
-        }else{
-
-        // retorno de variaveis gerais
-
-        $total_estagios = Estagio::get('id')->count();
-
-        $total_concluidos = Estagio::where('status','=','concluido')
-        ->orWhere('status','=','em_alteracao')->count();
-
-        $total_renovados = Estagio::where('renovacao_parent_id','!=',null)->count();
-
-        $total_rescindidos = Estagio::where('status','=','rescisao')->count();
-
-        $total_empresas = Empresa::get('id')->count();
-
-        $total_bachfilosofia = Estagio::where('graduacao','=','Bacharelado em Filosofia')->count();
-
-        $total_licfilosofia = Estagio::where('graduacao','=','Licenciatura em Filosofia')->count();
-
-        $total_bachhistoria = Estagio::where('graduacao','=','Bacharelado em Historia')->count();
-
-        $total_lichistoria = Estagio::where('graduacao','=','Licenciatura em Historia')->count();
-
-        $total_bachgeografia = Estagio::where('graduacao','=','Bacharelado em Geografia')->count();
-
-        $total_licgeografia = Estagio::where('graduacao','=','Licenciatura em Geografia')->count();
-
-        $total_bachsociais = Estagio::where('graduacao','=','Bacharelado em Ciencias Sociais')->count();
-
-        $total_licsociais = Estagio::where('graduacao','=','Licenciatura em Ciencias Sociais')->count();
-
-        $total_bachletras = Estagio::where('graduacao','LIKE',"%"."bacharelado - habilitacao"."%")->count();
-
-        $total_licletras = Estagio::where('graduacao','LIKE',"%"."licenciatura - habilitacao"."%")->count();
-
-        $total_basicoletras = Estagio::where('graduacao','=','Letras - Ciclo Básico')->count();
-
+        if($request->curso) {
+            $estagios = $estagios->where('nomcur',$request->curso);
         }
 
         return view('estatisticas.index')->with([
-            'total_estagios' => $total_estagios,
-            'total_concluidos' => $total_concluidos,
-            'total_renovados' => $total_renovados,
-            'total_rescindidos' => $total_rescindidos,
-            'total_empresas' => $total_empresas,
-            'busca_ano' => $request->busca_ano,
-            'total_bachfilosofia' => $total_bachfilosofia,
-            'total_licfilosofia' => $total_licfilosofia,
-            'total_bachhistoria' => $total_bachhistoria,
-            'total_lichistoria' => $total_lichistoria,
-            'total_bachgeografia' => $total_bachgeografia,
-            'total_licgeografia' => $total_licgeografia,
-            'total_bachsociais' => $total_bachsociais,
-            'total_licsociais' => $total_licsociais,
-            'total_bachletras' => $total_bachletras,
-            'total_licletras' => $total_licletras,
-            'total_basicoletras' => $total_basicoletras,
+            'cursos' => $cursos,
+            'estagios' => $estagios->get()
         ]);
     }
 }
--- resources/views/estatisticas/index.blade.php
+++ resources/views/estatisticas/index.blade.php
@@ -7,121 +7,63 @@
     <div class="card-header"><b>Filtrar estatísticas sobre o sistema</b></div>
     <div class="card-body">
 
-    <form method="get" action="/estatisticas">
-    @csrf
-        <div class="row">
-            <div class=" col-2 input-group">
-            <input type="text" maxlength="4" class="form-control" name="busca_ano" value="{{Request()->busca_ano}}" placeholder="Busca por ano (ex: 2020)"
-            oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');">  
+        <form method="get" action="/estatisticas">
+        @csrf
+            Estágios que iniciaram entre:
+            <div class="row">
+                <input class="datepicker" name="start_date" value="{{ request()->start_date }}"> 
+                    e
+                <input class="datepicker" name="end_date" value="{{ request()->end_date }}">
 
-            <span class="input-group-btn">
-                <button type="submit" class="btn btn-success"> Buscar </button>
-            </span>
+                <select name="curso" class="form-select">
+                    <option value="0" selected>Qualquer curso</option>
+                    @foreach($cursos as $curso)
+                        <option value="{{ $curso }}"> {{ $curso }} </option>
+                    @endforeach
+                </select>
+
+                <span class="input-group-btn">
+                    <button type="submit" class="btn btn-success"> Buscar </button>
+                </span>
             </div>
-        </div>
-    </form>
+        </form>
     </div>
 </div>
 
 <br>
 
+@if(isset($estagios))
 <div class="card">
     <div class="card-header">    
-        @if($busca_ano)
-        <b>Totais gerais da base de dados e novos estágios criados por curso - Ano de {{$busca_ano}}</b>
-        @else
-        <b>Totais gerais da base de dados e estágios por curso</b>
-        @endif
     </div>
 
     <div class="row">
-            <div class="col-sm form-group">
+    <div class="col-sm form-group">
                 <table class="table table-striped">
                     <tr>
                         <th>Estágios cadastrados:</th>
-                        <td>{{ $total_estagios }}</td>
+                        <td> {{ $estagios->whereNotIn('status',['em_elaboracao','cancelado'])->count() }} </td>
                     </tr>
-                    @if($busca_ano)
-                    <tr>
-                        <th>Estágios que estão/estiveram ativos neste ano:</th>
-                        <td>{{ $total_concluidos }}</td>
-                    </tr>
-                    @else
+                            
                     <tr>
                         <th>Estágios em andamento:</th>
-                        <td>{{ $total_concluidos }}</td>
+                        <td>{{ $estagios->whereNotIn('status',['em_elaboracao','cancelado', 'rescisao'])->count() }} </td>
                     </tr>
-                    @endif
+
                     <tr>
                         <th>Estágios renovados:</th>
-                        <td>{{ $total_renovados }}</td>
+                        <td> {{ $estagios->where('renovacao_parent_id')->count() }} </td>
                     </tr>
                     <tr>
                         <th>Estágios rescindidos:</th>
-                        <td>{{ $total_rescindidos }} </td>
-                    </tr>
-                    <tr>
-                        <th>Empresas cadastradas:</th>
-                        <td>{{ $total_empresas }}</td>
+                        <td>  {{ $estagios->where('status','rescisao')->count() }} </td>
                     </tr>
                 </table>
-            </div>
-            
-            <div class="col-sm form-group">
-                <table class="table table-striped">
-                    <tr>
-                        <th>Estágiarios do curso de Licenciatura em Ciências Sociais:</th>
-                        <td>{{ $total_licsociais }}</td> 
-                    </tr>
-                    <tr>
-                        <th>Estágiarios do curso de Bacharelado em Ciências Sociais:</th>
-                        <td>{{ $total_bachsociais }}</td> 
-                    </tr>
-                    <tr>
-                        <th>Estágiarios do curso de Licenciatura em Filosofia:</th>
-                        <td>{{ $total_licfilosofia }}</td>
-                    </tr>
-                    <tr>
-                        <th>Estágiarios do curso de Bacharelado em Filosofia:</th>
-                        <td>{{ $total_bachfilosofia }}</td>
-                    </tr>
-                    <tr>
-                        <th>Estágiarios do curso de Licenciatura em Geografia:</th>
-                        <td>{{ $total_licgeografia }}</td>
-                    </tr>
-                    <tr>
-                        <th>Estágiarios do curso de Bacharelado em Geografia:</th>
-                        <td>{{ $total_bachgeografia }}</td>
-                    </tr>
-                    <tr>
-                        <th>Estágiarios do curso de Licenciatura em História:</th>
-                        <td>{{ $total_lichistoria }}</td>
-                    </tr>
-                    <tr>
-                        <th>Estágiarios do curso de Bacharelado em História:</th>
-                        <td>{{ $total_bachhistoria }}</td>
-                    </tr>
-                    <tr>
-                        <th>Estágiarios dos cursos de Licenciatura em Letras:</th>
-                        <td>{{ $total_licletras }}</td>
-                    </tr>
-                    <tr>
-                        <th>Estágiarios dos cursos de Bacharelado em Letras:</th>
-                        <td>{{ $total_bachletras }}</td>
-                    </tr>
-                    <tr>
-                        <th>Estágiarios do ciclo básico em Letras:</th>
-                        <td>{{ $total_basicoletras }}</td>
-                    </tr>
-                </table>
-            </div>
-            
-        
-        </div>
+            </div>            
 
     </div>
 
     <br> Informações geradas ás {{ date("H:i"); }} do dia {{ date("d/m/Y")}} 
 </div>
-
+@endif
 @endsection('content')
