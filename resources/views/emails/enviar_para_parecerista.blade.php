044942f3264ed47002757be9720e56146c37c75e
Gabriel de Martin Truzzi <gabriel.truzzi@gmail.com>
2022-03-09 16:34:34 -0300

    correção dos textos de emails

--- resources/views/emails/enviar_para_parecerista.blade.php
+++ resources/views/emails/enviar_para_parecerista.blade.php
@@ -1,13 +1,6 @@
-Segue em anexo o parecer de mérito relativo ao estágio de {{ $estagio->nome }}, Nº USP {{ $estagio->numero_usp }}<br><br>
-
-
-Parecer Realizado por: {{ $estagio->parecerista_nome }} <br><br>
-
-
-Acesse o sistema de estágio https://estagios.fflch.usp.br/ com senha única USP e vá na opção
-"Estágios para Parecer de Mérito".
-
-
+Segue em anexo o parecer de mérito relativo ao estágio de {{ $estagio->nome }}, Nº USP {{ $estagio->numero_usp }}
+<br><br>
+Parecer Realizado por: {{ $estagio->parecerista_nome }}. 
 <br><br>
 Mensagem automática - Sistema de Estágios - FFLCH-USP
 
