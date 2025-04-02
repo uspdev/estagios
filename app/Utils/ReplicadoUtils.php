f71f73576c1d27e6d594b80423504d7d487b11ca
Ricardo Fontoura <ricardfo@usp.br>
2025-04-02 07:51:11 -0300

    ajustando a consulta do periodo.

--- app/Utils/ReplicadoUtils.php
+++ app/Utils/ReplicadoUtils.php
@@ -13,7 +13,7 @@ class ReplicadoUtils {
      * @return int
      */
     public static function semestreAtual($codpes) {
-        $query = "SELECT COUNT(s.anosem) FROM SITALUNOATIVOGR s 
+        $query = "SELECT COUNT(s.anosem) FROM SITALUNOATIVOGR s
                     WHERE codpes = convert(int,:codpes)";
         $param = [
             'codpes' => $codpes,
@@ -29,7 +29,7 @@ class ReplicadoUtils {
     public static function grade($codpes){
         $current = date("Y") . (date("m") > 6? 2:1);
 
-        $query =  "SELECT h.coddis, h.codtur, o.diasmnocp, p.horent, p.horsai FROM HISTESCOLARGR h 
+        $query =  "SELECT h.coddis, h.codtur, o.diasmnocp, p.horent, p.horsai FROM HISTESCOLARGR h
         INNER JOIN OCUPTURMA o ON (h.coddis = o.coddis AND h.codtur = o.codtur)
         INNER JOIN PERIODOHORARIO p ON (o.codperhor = p.codperhor)
         where h.codpes = convert(int,:codpes) and h.codtur LIKE '%{$current}%' ORDER BY o.diasmnocp";
@@ -40,19 +40,19 @@ class ReplicadoUtils {
     }
 
     public static function periodo($codpes){
-        $query = "SELECT perhab FROM HABILITACAOGR h 
-        INNER JOIN HABILPROGGR r
-        ON r.codhab = h.codhab
-        WHERE r.codpes = convert(int,:codpes)
-        ORDER BY r.dtaini DESC";
-        
+        $query = "SELECT H.perhab FROM VINCULOPESSOAUSP V
+        INNER JOIN CURSOGR C ON (V.codcurgrd = C.codcur)
+        INNER JOIN HABILITACAOGR H ON (H.codhab = V.codhab)
+        WHERE V.codpes = convert(int, :codpes) AND V.tipvin = 'ALUNOGR'
+        AND V.codcurgrd = H.codcur AND V.codhab = H.codhab";
+
         $param = [
             'codpes' => $codpes,
         ];
+
         $result = DBreplicado::fetch($query, $param);
 
-        if($result) return $result['perhab'];
-        else return null;
-    } 
+        return $result['perhab'];
+    }
 
 }
